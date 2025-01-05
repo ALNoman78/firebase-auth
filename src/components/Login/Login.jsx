0763360e6e6548c47aa5f9e0695ddecc0cec1c3b
ALNoman78 <abdullaalno078@gmail.com>
2025-01-05 22:38:23 +0600

    twitter authentication not working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,40 +1,50 @@
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut, TwitterAuthProvider } from "firebase/auth"
 import auth from "../../firebase.init"
-import { useDebugValue, useState } from "react"
+import { useState } from "react"
 
 
 const Login = () => {
-    const [user , setUser] = useState(null)
+    const [user, setUser] = useState(null)
 
     const googleProvider = new GoogleAuthProvider()
     const githubProvider = new GithubAuthProvider()
+    const twitterProvider = new TwitterAuthProvider()
+
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth , googleProvider)
-        .then((request) => {
-            console.log(request);
-            setUser(request.user)
-        })
-        .catch(error => console.log(error))
+        signInWithPopup(auth, googleProvider)
+            .then((request) => {
+                console.log(request);
+                setUser(request.user)
+            })
+            .catch(error => console.log(error))
     }
 
     const handleSignOut = () => {
         signOut(auth)
-        .then(() => {
-            console.log('successful')
-            setUser(null)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(() => {
+                console.log('successful')
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     const handleGithubSignIn = () => {
-        signInWithPopup(auth , githubProvider)
-        .then((result => {
-            // const credential = GithubAuthProvider.credentialFromResult(result)
-            console.log(result.user)
-            setUser(result.user)
-        }))
-        .catch(error => console.log(error))
+        signInWithPopup(auth, githubProvider)
+            .then((result => {
+                // const credential = GithubAuthProvider.credentialFromResult(result)
+                console.log(result.user)
+                setUser(result.user)
+            }))
+            .catch(error => console.log(error))
+    }
+
+    const handleTwitterSignIn = () => {
+        signInWithPopup(auth, twitterProvider)
+            .then((result) => {
+                setUser(result.user)
+            })
+            .catch(error => console.log(error))
     }
     return (
         <div className="flex justify-center my-6">
@@ -43,16 +53,17 @@ const Login = () => {
                 user ?
                     <button onClick={handleSignOut} className="btn ml-3 btn-error">Sign Out</button>
                     :
-                    <div>   
+                    <div>
                         <button onClick={handleGoogleSignIn} className="btn btn-accent">Continue with Google</button>
                         <button onClick={handleGithubSignIn} className="btn btn-outline btn-success">Continue with Github</button>
+                        <button onClick={handleTwitterSignIn} className="btn btn-accent">Sign In with twitter</button>
                     </div>
             }
             {
                 user && <div>
                     <img className="rounded-full w-14 h-14" src={user.photoURL} alt="" />
                     <h4>{user.displayName}</h4>
-                    <p>Email address : {user.email}</p>
+                    <p>{user?.email}</p>
                 </div>
             }
         </div>
