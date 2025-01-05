b9a517c495fa57aafc61977f27a33095bc5e915f
ALNoman78 <abdullaalno078@gmail.com>
2025-01-05 19:10:39 +0600

    github authorization added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,44 +1,53 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
 import auth from "../../firebase.init"
-import { useState } from "react"
+import { useDebugValue, useState } from "react"
 
 
 const Login = () => {
+    const [user , setUser] = useState(null)
 
-    const provider = new GoogleAuthProvider
-    const [user, setUser] = useState(null)
-
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
-            .then((request) => {
-                console.log(request.user)
-                setUser(request.user)
-            })
-            .catch((error) => {
-                console.log('Error', error)
-                setUser(null)
-            })
+        signInWithPopup(auth , googleProvider)
+        .then((request) => {
+            console.log(request);
+            setUser(request.user)
+        })
+        .catch(error => console.log(error))
     }
 
     const handleSignOut = () => {
         signOut(auth)
-            .then((resutl) => {
-                console.log('Sign out successful', resutl);
-                setUser(null)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+        .then(() => {
+            console.log('successful')
+            setUser(null)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth , githubProvider)
+        .then((result => {
+            // const credential = GithubAuthProvider.credentialFromResult(result)
+            console.log(result.user)
+            setUser(result.user)
+        }))
+        .catch(error => console.log(error))
     }
     return (
         <div className="flex justify-center my-6">
             {/* <button onClick={handleGoogleSignIn} className="btn btn-accent">Continue with Google</button> */}
             {
                 user ?
-                    <button onClick={handleSignOut} className="btn ml-3 btn-error">Sign Out</button> :
-                    <button onClick={handleGoogleSignIn} className="btn btn-accent">Continue with Google</button>
+                    <button onClick={handleSignOut} className="btn ml-3 btn-error">Sign Out</button>
+                    :
+                    <div>   
+                        <button onClick={handleGoogleSignIn} className="btn btn-accent">Continue with Google</button>
+                        <button onClick={handleGithubSignIn} className="btn btn-outline btn-success">Continue with Github</button>
+                    </div>
             }
-            {/* <button onClick={handleSignOut} className="btn ml-3 btn-error">Sign Out</button> */}
             {
                 user && <div>
                     <img className="rounded-full w-14 h-14" src={user.photoURL} alt="" />
