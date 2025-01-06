e20ef83316596ad40735680b2149c85e05b17eb5
ALNoman78 <abdullaalno078@gmail.com>
2025-01-06 22:39:25 +0600

    form added

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const Form = () => {
+    const [name , setName] = useState(null)
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(name.target.value)
+        console.log(e.target.name.value)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={setName} type="text" name="name" id="" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+Form.propTypes = {}
+
+export default Form
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
+import Form from "../Form/Form";
+
 const Home = () => {
     return (
         <div>
             <h2>This  is home</h2>
+            <Form></Form>
         </div>
     );
 };
