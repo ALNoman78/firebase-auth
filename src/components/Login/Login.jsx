54c6fb22f589a25da642e5e5ae07a31072572ee9
ALNoman78 <abdullaalno078@gmail.com>
2025-01-29 22:59:19 +0600

    remove auth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut, TwitterAuthProvider } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth"
 import auth from "../../firebase.init"
 import { useState } from "react"
 import { Link } from "react-router-dom"
